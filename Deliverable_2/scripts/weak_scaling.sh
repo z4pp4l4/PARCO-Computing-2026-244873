80b7191379b4a703288f2a2ef6ee00972dd6a9b6
z4pp4l4 <145671863+z4pp4l4@users.noreply.github.com>
2026-01-13 12:40:38 +0100

    unified 1d-2d weak scaling

--- Deliverable_2/scripts/weak_scaling.sh
+++ Deliverable_2/scripts/weak_scaling.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # Weak scaling benchmark (file-based, clean)
 
-SRC=./MPI_impl_2D_partitioning.c
-EXEC=./MPI_impl_2D_partitioning.out
+SRC1=./MPI_impl_2D_partitioning.c
+EXEC1=./MPI_impl_2D_partitioning.out
+
+SRC2=./MPI_implementation.c
+EXEC2=./MPI_implementation.out
 
 PROCS=(1 2 4 8 16 32 64 128)
 MATRIX_DIR=../src/matrices
 
 # Compile once
 echo "Compiling SpMV code..."
-mpicc -O3 -std=c99 -fopenmp "${SRC}" -o "${EXEC}" || exit 1
+mpicc -O3 -std=c99 -fopenmp "${SRC1}" -o "${EXEC1}" || exit 1
+mpicc -O3 -std=c99 -fopenmp "${SRC2}" -o "${EXEC2}" || exit 1
+
 echo "Compilation OK"
 echo
 
+echo "****************WEAK SCALING 2D*********************"
+echo
 for P in "${PROCS[@]}"; do
     MTX="${MATRIX_DIR}/weak_ps${P}.mtx"
 
@@ -25,5 +32,21 @@ for P in "${PROCS[@]}"; do
     echo "P=${P} | Matrix=$(basename ${MTX})"
     echo "---------------------------------------------"
 
-    mpirun -np "${P}" "${EXEC}" "${MTX}"
+    mpirun -np "${P}" "${EXEC1}" "${MTX}"
+done
+echo
+echo "****************WEAK SCALING 1D*********************"
+for P in "${PROCS[@]}"; do
+    MTX="${MATRIX_DIR}/weak_ps${P}.mtx"
+
+    if [ ! -f "${MTX}" ]; then
+        echo "[ERROR] Missing matrix ${MTX}"
+        exit 1
+    fi
+
+    echo "---------------------------------------------"
+    echo "P=${P} | Matrix=$(basename ${MTX})"
+    echo "---------------------------------------------"
+
+    mpirun -np "${P}" "${EXEC2}" "${MTX}"
 done
